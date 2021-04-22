f445eaca7667c5f2136da3d10a8e6a0728a003d9
nation430 <tonyenation430@gmail.com>
2021-04-22 23:21:07 +0100

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,20 @@
+const newYears = "1 Jan 2021";
+
+function countdown(){
+      const newYearsDate = new Date(newYears);
+      const currentDate = new Date();
+
+      const total = (newYearsDate -  currentDate) * 1000;
+
+      const days = Math.floor(total / 3600 / 24);
+
+
+
+      console.log(newYearsDate -  currentDate);
+}
+
+//Initial call
+
+countdown();
+
+setInterval(countdown, 1000);
